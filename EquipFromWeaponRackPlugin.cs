aa2dce0f462baaccdfae1dc6b9f8031a67da733e
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2024-03-31 15:27:11 -0700

    Update version

--- EquipFromWeaponRackPlugin.cs
+++ EquipFromWeaponRackPlugin.cs
@@ -3,7 +3,7 @@ using DrakiaXYZ.EquipFromWeaponRack.Patches;
 
 namespace DrakiaXYZ.EquipFromWeaponRack
 {
-    [BepInPlugin("xyz.drakia.equipfromweaponrack", "DrakiaXYZ-EquipFromWeaponRack", "1.0.1")]
+    [BepInPlugin("xyz.drakia.equipfromweaponrack", "DrakiaXYZ-EquipFromWeaponRack", "1.1.0")]
     public class EquipFromWeaponRackPlugin : BaseUnityPlugin
     {
         public EquipFromWeaponRackPlugin()
